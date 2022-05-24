72fc3f673db00d7e07d9feef1d76c9f13eb413f1
Leviathan-bit <84622086+Leviathan-bit@users.noreply.github.com>
2022-05-24 15:00:55 +0700

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -25,7 +25,7 @@ apt install nala -y
 
 # Install XFCE Desktop
 nala upgrade -y
-nala install xfce4-appfinder xfce4-battery-plugin xfce4-datetime-plugin xfce4-diskperf-plugin xfce4-fsguard-plugin xfce4-genmon-plugin xfce4-mount-plugin xfce4-netload-plugin xfce4-notifyd xfce4-panel xfce4-places-plugin xfce4-power-manager xfce4-power-manager-data xfce4-power-manager-plugins xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-session xfce4-settings xfce4-systemload-plugin xfce4-taskmanager xfce4-terminal xfce4-timer-plugin xfce4-verve-plugin xfce4-wavelan-plugin xfce4-xkb-plugin xfwm4 xfconf xfdesktop4 xfdesktop4-data thunar-archive-plugin thunar-shares-plugin thunar-data thunar-gtkhash thunar-volman thunar-custom-actions-mx network-manager network-manager-gnome network-manager-openvpn network-manager-openvpn-gnome xfce4-appmenu-plugin brave-browser plank ark mx-repo-manager mx-packageinstaller mx-remaster fonts-noto-color-emoji fonts-firacode fonts-font-awesome libqt5svg5 qml-module-qtquick-controls unzip lxappearance -y
+nala install xfce4-appfinder xfce4-battery-plugin xfce4-datetime-plugin xfce4-diskperf-plugin xfce4-fsguard-plugin xfce4-genmon-plugin xfce4-mount-plugin xfce4-netload-plugin xfce4-notifyd xfce4-panel xfce4-places-plugin xfce4-power-manager xfce4-power-manager-data xfce4-power-manager-plugins xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-session xfce4-settings xfce4-systemload-plugin xfce4-taskmanager xfce4-terminal xfce4-timer-plugin xfce4-verve-plugin xfce4-wavelan-plugin xfce4-xkb-plugin xfwm4 xfconf xfdesktop4 xfdesktop4-data thunar-archive-plugin thunar-shares-plugin thunar-data thunar-gtkhash thunar-volman thunar-custom-actions-mx network-manager network-manager-gnome network-manager-openvpn network-manager-openvpn-gnome xfce4-appmenu-plugin brave-browser plank ark mx-repo-manager mx-packageinstaller mx-remaster fonts-noto-color-emoji fonts-firacode fonts-font-awesome libqt5svg5 qml-module-qtquick-controls unzip lxappearance ttf-mscorefonts-installer -y
 
 # Deb install 
 dpkg -i 'deb/ulauncher.deb'
@@ -35,8 +35,7 @@ echo 'GRUB_DISABLE_OS_PROBER=false' >> /etc/default/grub
 update-grub
 
 # Download Nordic Theme
-#cd /usr/share/themes/
-#git clone https://github.com/EliverLara/Nordic.git
+git clone https://github.com/EliverLara/Nordic.git /usr/share/themes/Nordic
 
 # Fira Code Nerd Font variant needed
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v1.1.0/FiraCode.zip
@@ -44,3 +43,10 @@ unzip FiraCode.zip -d /usr/share/fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v1.1.0/Meslo.zip
 unzip Meslo.zip -d /usr/share/fonts
 fc-cache -vf
+
+unzip icon.zip -d /home/$USER
+unzip config.zip -d /home/$USER
+rm -rf /usr/share/backgrounds
+unzip backgrounds.zip -d /usr/share/
+cp .gtkrc-2.0 /home/$username
+chown -R $USER:$USER /home/$USER
